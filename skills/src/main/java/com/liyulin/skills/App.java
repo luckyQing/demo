997d21e0fd12e937b73a1c05b67dadf768e79f92
luckyQing <1634753825@qq.com>
2018-10-07 21:02:03 +0800

    .

---
+++ skills/src/main/java/com/liyulin/skills/App.java
@@ -0,0 +1,13 @@
+package com.liyulin.skills;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
