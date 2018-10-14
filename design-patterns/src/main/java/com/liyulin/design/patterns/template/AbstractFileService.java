a595545015559b5b5622258e9d157f232029b8e1
luckyQing <1634753825@qq.com>
2018-10-14 23:39:50 +0800

    .

--- design-patterns/src/main/java/com/liyulin/design/patterns/template/AbstractFileService.java
+++ design-patterns/src/main/java/com/liyulin/design/patterns/template/AbstractFileService.java
@@ -4,6 +4,14 @@ import com.liyulin.design.patterns.template.third.IFileService;
 
 import lombok.extern.slf4j.Slf4j;
 
+/**
+ * 模板方法模式
+ * <p>
+ * 在一个方法中定义一个算法的骨架，而将一些步骤的实现延迟到子类中。模板方法使得子类可以在不改变算法结构的情况下，重新定义算法中某些步骤的具体实现。
+ *
+ * @author liyulin
+ * @date 2018年10月14日下午11:39:24
+ */
 @Slf4j
 public abstract class AbstractFileService {
 
@@ -20,7 +28,7 @@ public abstract class AbstractFileService {
 		insertLog();
 		return true;
 	}
-	
+
 	private boolean validate() {
 		log.info("参数校验");
 		return true;
