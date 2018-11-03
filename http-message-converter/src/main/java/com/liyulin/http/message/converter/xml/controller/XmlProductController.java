30f76102cd650fe2aeb1206cf719965ac974c7e3
luckyQing <1634753825@qq.com>
2018-11-03 11:23:57 +0800

    remove consumer

--- http-message-converter/src/main/java/com/liyulin/http/message/converter/xml/controller/XmlProductController.java
+++ http-message-converter/src/main/java/com/liyulin/http/message/converter/xml/controller/XmlProductController.java
@@ -1,6 +1,5 @@
 package com.liyulin.http.message.converter.xml.controller;
 
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,7 +11,7 @@ import com.liyulin.http.message.converter.xml.dto.XmlResult;
 @RestController
 public class XmlProductController {
 
-	@PostMapping(value = "/xml", consumes = MediaType.APPLICATION_XML_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
+	@PostMapping(value = "/xml")
 	public XmlResult<XmlProductDto> xml(@RequestBody XmlProductDto xmlProductDto) {
 		return new XmlResult<>(ReturnCodeEnum.SUCCESS, xmlProductDto);
 	}
