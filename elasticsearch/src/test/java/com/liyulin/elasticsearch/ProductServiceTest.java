ee9c87ea25c471a18aefbe7fc09d9663036863d2
luckyQing <1634753825@qq.com>
2019-03-02 12:29:05 +0800

    .

--- elasticsearch/src/test/java/com/liyulin/elasticsearch/ProductServiceTest.java
+++ elasticsearch/src/test/java/com/liyulin/elasticsearch/ProductServiceTest.java
@@ -4,13 +4,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.liyulin.elasticsearch.service.ProductService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
 public class ProductServiceTest {
