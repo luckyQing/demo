e1d2d42055842a50a4bda99c6bcd2dfbe0f40010
luckyQing <1634753825@qq.com>
2019-06-04 13:23:46 +0800

    完善

--- sharding-jdbc/README.md
+++ sharding-jdbc/README.md
@@ -1,1 +1,9 @@
 # sharding-jdbc实例
+## 一、文档
+```
+https://shardingsphere.apache.org/document/current/en/overview/
+```
+## 二、注意事项
+```
+1、sharding-jdbc默认不支持动态创建表，需要预先建好需要的表。
+```
---
+++ sharding-jdbc/src/main/java/com/liyulin/shading/jdbc/demo/algorithm/WeekPreciseShardingAlgorithm.java
@@ -0,0 +1,34 @@
+package com.liyulin.shading.jdbc.demo.algorithm;
+
+import java.util.Collection;
+import java.util.Date;
+
+import org.joda.time.DateTime;
+
+import io.shardingsphere.api.algorithm.sharding.PreciseShardingValue;
+import io.shardingsphere.api.algorithm.sharding.standard.PreciseShardingAlgorithm;
+
+/**
+ * 按周分片
+ * 
+ * @author liyulin
+ * @date 2019年6月3日 下午9:29:10
+ */
+public class WeekPreciseShardingAlgorithm implements PreciseShardingAlgorithm<Date> {
+
+	@Override
+	public String doSharding(Collection<String> availableTargetNames, PreciseShardingValue<Date> shardingValue) {
+		DateTime dateTime = new DateTime(shardingValue.getValue().getTime());
+		int year = dateTime.getYear();
+		int week = dateTime.getWeekOfWeekyear();
+		// t_product_info_2019_23
+		StringBuilder tableName = new StringBuilder(32);
+		tableName.append(shardingValue.getLogicTableName())
+				 .append("_")
+				 .append(year)
+				 .append("_")
+				 .append(week);
+		return tableName.toString();
+	}
+
+}
