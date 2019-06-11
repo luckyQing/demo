5f4d68a81f35449b3a333bda380a37738c275dc9
luckyQing <1634753825@qq.com>
2019-06-11 22:26:46 +0800

    添加grpc demo

---
+++ grpc-module/grpc-provider/src/main/java/com/liyulin/grpc/provider/App.java
@@ -0,0 +1,13 @@
+package com.liyulin.grpc.provider;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
