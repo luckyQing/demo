90b7c9e95bcc4eee4fc4ae91237945fb56412700
luckyQing <1634753825@qq.com>
2021-06-11 19:14:51 +0800

    update sql

--- shiro-servlet/sql/demo_shiro.sql
+++ shiro-servlet/sql/demo_shiro.sql
@@ -1,9 +1,3 @@
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
-
--- ----------------------------
--- Table structure for permission
--- ----------------------------
 DROP TABLE IF EXISTS `permission`;
 CREATE TABLE `permission`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -12,17 +6,11 @@ CREATE TABLE `permission`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 5 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of permission
--- ----------------------------
 INSERT INTO `permission` VALUES (1, 'add', '增加');
 INSERT INTO `permission` VALUES (2, 'update', '更新');
 INSERT INTO `permission` VALUES (3, 'select', '查看');
 INSERT INTO `permission` VALUES (4, 'delete', '删除');
 
--- ----------------------------
--- Table structure for role
--- ----------------------------
 DROP TABLE IF EXISTS `role`;
 CREATE TABLE `role`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -31,27 +19,20 @@ CREATE TABLE `role`  (
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of role
--- ----------------------------
 INSERT INTO `role` VALUES (1, 'admin', '超级管理员');
 INSERT INTO `role` VALUES (2, 'user', '普通用户');
 INSERT INTO `role` VALUES (3, 'vip_user', 'VIP用户');
 
--- ----------------------------
--- Table structure for role_permission
--- ----------------------------
 DROP TABLE IF EXISTS `role_permission`;
 CREATE TABLE `role_permission`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `role_id` int(11) NULL DEFAULT NULL,
   `permission_id` int(255) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_role_id`(`role_id`) USING BTREE,
+  INDEX `idx_permission_id`(`permission_id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 9 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
--- ----------------------------
--- Records of role_permission
--- ----------------------------
 INSERT INTO `role_permission` VALUES (1, 1, 1);
 INSERT INTO `role_permission` VALUES (2, 1, 2);
 INSERT INTO `role_permission` VALUES (3, 1, 3);
@@ -61,39 +42,30 @@ INSERT INTO `role_permission` VALUES (6, 3, 3);
 INSERT INTO `role_permission` VALUES (7, 3, 2);
 INSERT INTO `role_permission` VALUES (8, 2, 1);
 
--- ----------------------------
--- Table structure for user
--- ----------------------------
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `username` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `account` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE INDEX `uk_account`(`account`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
--- ----------------------------
--- Records of user
--- ----------------------------
 INSERT INTO `user` VALUES (1, 'root', '超级用户', 'root');
 INSERT INTO `user` VALUES (2, 'user', '普通用户', 'user');
 INSERT INTO `user` VALUES (3, 'vip', 'VIP用户', 'vip');
 
--- ----------------------------
--- Table structure for user_role
--- ----------------------------
 DROP TABLE IF EXISTS `user_role`;
 CREATE TABLE `user_role`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NULL DEFAULT NULL,
   `role_id` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
+  PRIMARY KEY (`id`) USING BTREE,
+  INDEX `idx_user_id`(`user_id`) USING BTREE,
+  INDEX `idx_role_id`(`role_id`) USING BTREE
 ) ENGINE = MyISAM AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Fixed;
 
--- ----------------------------
--- Records of user_role
--- ----------------------------
 INSERT INTO `user_role` VALUES (1, 1, 1);
 INSERT INTO `user_role` VALUES (2, 2, 2);
 INSERT INTO `user_role` VALUES (3, 3, 3);
